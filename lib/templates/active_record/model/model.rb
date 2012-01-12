130fb81dc73f60cbdc3fe0742d4190efa9d75bc8
Marcin Raczkowski <marcin.raczkowski@gmail.com>
2012-01-12 10:03:56 +0100

    Adding a modified model template

---
+++ lib/templates/active_record/model/model.rb
@@ -0,0 +1,24 @@
+<% module_namespacing do -%>
+# encoding: utf-8
+class <%= class_name %> < <%= parent_class_name.classify %>
+<% attributes.select {|attr| attr.reference? }.each do |attribute| -%>
+  belongs_to :<%= attribute.name %>
+<% end -%>
+
+  column :id, :integer
+<% attributes.reject {|attr| attr.reference? }.each do |attr| -%>
+  column :<%= attr.name %>, :<%= attr.type %>
+<% end -%>
+<% {
+  "Validations" => "Declarations of model required structure.",
+  "Named Scopes" => "Scopes and methods for narrowing down SQL Queries", 
+  "Class Methods" => "Class methods not related to finding records.", 
+  "Initialization" => "Declarations of initialization commands (creation procedures/constructors)", 
+  "Access" => "Declarations of non-boolean queries on the object state, e.g. item",
+  "Status report" => "Declarations of boolean queries on the object state, e.g. is_empty?",
+  "Element change" => "eclarations of commands that change the structure, e.g. update_count!"
+}.each do |name, description| %>
+<%= "#### %-14s - %-75s ####" % [name, description] %>
+<% end %>
+end
+<% end -%>
